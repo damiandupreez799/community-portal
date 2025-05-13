ffb1e03c688ec1d30091642f849bc85ed78a96f4
Thapelo-Mphahlele <thapelo.mphahlele@hisgroup-it.co.za>
2025-05-13 12:52:57 +0200

    changed pageRoutes to be able to view pages

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,18 +4,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.render('pages/home', {title: 'Home'});
 });
 
 router.get('/about', (req, res) => {
+    res.render('pages/about', {title: 'About Us'});
 });
 
 router.get('/events', (req, res) => {
+    res.render('pages/events', {title:'Events'});
 });
 
 router.get('/contact', (req, res) => {
+    res.render('pages/contact', {title: 'Contact Us'});
 });
 
 router.get('/thankyou', (req, res) => {
+    res.render('pages/thankyou', {title: 'Thank You'});
 });
 
 module.exports = router;
